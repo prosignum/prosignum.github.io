efdb08bfc67ad139592abc128e6b7fa98c7ee608
Your Name <you@example.com>
2022-08-05 00:09:43 +0300

    BrowserRouter changed to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Nav from './components/nav/nav';
 import { navItems } from './components/nav/navItems';
 import BackgroundA from './components/animations/backgroundA';
@@ -7,14 +7,14 @@ import BackgroundA from './components/animations/backgroundA';
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Nav />
         <Routes>
           {
             navItems.map(item => <Route key={item.id} path={item.path} element={item.component} />)
           }
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <BackgroundA />
     </>
   )
